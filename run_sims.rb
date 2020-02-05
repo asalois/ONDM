68ec49804c6fe90a4fdf807d8c59062a785b78f8
Alexander Salois <asalois.scholar@gmail.com>
2020-02-04 17:18:02 -0700

    set up a test run of a bash script from ruby from loops

--- run_sims.rb
+++ run_sims.rb
@@ -1,1 +1,7 @@
-This file will run similations with different parameters 
+# This file will run similations with different parameters 
+
+for i in 0..10
+    for j in  0..10
+        system("bash test.sh " + i.to_s + " " +j.to_s)
+    end
+end
