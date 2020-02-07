c006ba5edd3935a4d4e008e4d727630bd5b5b056
Alexander Salois <asalois.scholar@gmail.com>
2020-02-06 17:52:36 -0700

    added time on the scripts

--- test/test.sh
+++ test/test.sh
@@ -2,5 +2,5 @@
 # get and print inputs
 
 echo "Run a matlab script"
-matlab -nodesktop -nodisplay -nosplash -r 'testwrapper; exit;'
+time matlab -nodesktop -nodisplay -nosplash -r 'testwrapper; exit;'
 #time matlab -nodesktop -nodisplay -nosplash -r 'testwrapper_par; exit;'
