7f9052a73eceadb61f9962d19040558b6a651ac2
Alexander Salois <asalois.scholar@gmail.com>
2020-02-28 11:02:07 -0700

    made script that run about 4 and half minutes

--- test/testwrapper.m
+++ test/testwrapper.m
@@ -1,24 +1,12 @@
 % test
 
-for l = 8:32
+for l = 6:28
     bit_pwr = l
     run_to = 2^bit_pwr
-
-    % parallel test
-    tic
-    parfor i = 0:run_to
-        test_add(i, i);
-        x = test_exponent(i, 4);
-        test_divide(x,i);
-    end
-    toc
-
     % serial test
-    tic
     for i = 0:run_to
         test_add(i, i);
         x = test_exponent(i, 4);
         test_divide(x,i);
     end
-    toc
 end
