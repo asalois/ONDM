d9e2512267f8b83188d0088419061afe6e237518
Alexander Salois <asalois.scholar@gmail.com>
2020-02-28 11:02:07 -0700

    made script that run about 4 and half minutes

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # get and print inputs
-
+# takes about 4.5 minutes to run on XPS laptop
 echo "Run a matlab script"
 time matlab -nodesktop -nodisplay -nosplash -r 'testwrapper; exit;'
 #time matlab -nodesktop -nodisplay -nosplash -r 'testwrapper_par; exit;'
