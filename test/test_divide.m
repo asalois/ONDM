8affefe1951c5bf8c03622b19acd3340330325d1
Alexander Salois <asalois.scholar@gmail.com>
2020-03-04 14:00:46 -0700

    made a ruby template to make slurm files

--- test/test_divide.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1] = test(inputArg1,inputArg2)
-% test
-outputArg1 = inputArg1 / inputArg2;
-%formatSpec =  '%d / %d = %d \n';
-%fprintf(formatSpec, inputArg1, inputArg2, outputArg1)
-end
-
