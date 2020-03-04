cd4a169b99e1502268c63ace93e22026d8084c70
Alexander Salois <asalois.scholar@gmail.com>
2020-03-04 14:42:53 -0700

    added leading zeros

--- test/make_jobs/job.rb
+++ test/make_jobs/job.rb
@@ -2,11 +2,11 @@ class Job
     def initialize
       yield self if block_given?
     end
-  
+
     def queue
       @queue = "express"
     end
-  
+
     def name
       @name
     end
@@ -14,9 +14,29 @@ class Job
     def name=(in_name)
       @name = in_name
     end
-  
+
     def mem
       @mem = "2000"
+    end
+
+    def nodes
+      @nodes = 1
+    end
+
+    def tasks
+      @tasks = 1
+    end
+
+    def time
+      @time = "00:05:00"
+    end
+
+    def param1
+      @param1
+    end
+
+    def param1=(in_para)
+      @param1 = in_para
     end
 
   end
