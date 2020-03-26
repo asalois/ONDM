7d35ab23de5578504195fdc0a3dcd0603d88cf0b
Alex Salois <asalois.scholar@gmail.com>
2020-03-26 11:46:30 -0600

    cleaning up

--- test/output/power_test03/avg_time.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep "Elapsed time is"* *.out.txt -h | cut -d . -f 1 | cut -c 17-23 > run_time.txt 
-cat run_time.txt | py --ji -l 'min(l)/3600, max(l)/3600, numpy.median(l)/3600, numpy.mean(l)/3600'
