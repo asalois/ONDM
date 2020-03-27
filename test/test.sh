c7687b954b464bd33a20a4d8c282a10329fcadd4
Alex Salois <asalois.scholar@gmail.com>
2020-03-26 23:27:46 -0600

    cleaning up

--- test/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# runs a matlab script
-echo "Run a matlab script"
-time matlab -nodesktop -nodisplay -nosplash -r 'testwrapper; exit;'
--- test/testwrapper.m
+++
@@ -1,8 +0,0 @@
-% test
-clear all
-close all
-clc
-
-tic
-out = test(10);
-toc
