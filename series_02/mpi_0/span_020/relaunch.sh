bce9bed38580bc7e132c3adac72364b980dc5016
Alex Salois <asalois.scholar@gmail.com>
2020-05-19 14:32:49 -0600

    run 2 finished

---
+++ series_02/mpi_0/span_020/relaunch.sh
@@ -0,0 +1,461 @@
+#!/bin/bash
+# to relaunch jobs
+sbatch --array=1 qsmf_test_1.slurm
+sleep 1
+sbatch --array=5 qsmf_test_1.slurm
+sleep 1
+sbatch --array=6 qsmf_test_1.slurm
+sleep 1
+sbatch --array=8 qsmf_test_1.slurm
+sleep 1
+sbatch --array=9 qsmf_test_1.slurm
+sleep 1
+sbatch --array=10 qsmf_test_1.slurm
+sleep 1
+sbatch --array=11 qsmf_test_1.slurm
+sleep 1
+sbatch --array=12 qsmf_test_1.slurm
+sleep 1
+sbatch --array=13 qsmf_test_1.slurm
+sleep 1
+sbatch --array=14 qsmf_test_1.slurm
+sleep 1
+sbatch --array=15 qsmf_test_1.slurm
+sleep 1
+sbatch --array=16 qsmf_test_1.slurm
+sleep 1
+sbatch --array=18 qsmf_test_1.slurm
+sleep 1
+sbatch --array=19 qsmf_test_1.slurm
+sleep 1
+sbatch --array=20 qsmf_test_1.slurm
+sleep 1
+sbatch --array=21 qsmf_test_1.slurm
+sleep 1
+sbatch --array=22 qsmf_test_1.slurm
+sleep 1
+sbatch --array=23 qsmf_test_1.slurm
+sleep 1
+sbatch --array=24 qsmf_test_1.slurm
+sleep 1
+sbatch --array=26 qsmf_test_1.slurm
+sleep 1
+sbatch --array=28 qsmf_test_1.slurm
+sleep 1
+sbatch --array=29 qsmf_test_1.slurm
+sleep 1
+sbatch --array=30 qsmf_test_1.slurm
+sleep 1
+sbatch --array=31 qsmf_test_1.slurm
+sleep 1
+sbatch --array=32 qsmf_test_1.slurm
+sleep 1
+sbatch --array=33 qsmf_test_1.slurm
+sleep 1
+sbatch --array=35 qsmf_test_1.slurm
+sleep 1
+sbatch --array=36 qsmf_test_1.slurm
+sleep 1
+sbatch --array=37 qsmf_test_1.slurm
+sleep 1
+sbatch --array=38 qsmf_test_1.slurm
+sleep 1
+sbatch --array=39 qsmf_test_1.slurm
+sleep 1
+sbatch --array=41 qsmf_test_1.slurm
+sleep 1
+sbatch --array=43 qsmf_test_1.slurm
+sleep 1
+sbatch --array=45 qsmf_test_1.slurm
+sleep 1
+sbatch --array=46 qsmf_test_1.slurm
+sleep 1
+sbatch --array=47 qsmf_test_1.slurm
+sleep 1
+sbatch --array=48 qsmf_test_1.slurm
+sleep 1
+sbatch --array=49 qsmf_test_1.slurm
+sleep 1
+sbatch --array=50 qsmf_test_1.slurm
+sleep 1
+sbatch --array=51 qsmf_test_1.slurm
+sleep 1
+sbatch --array=52 qsmf_test_1.slurm
+sleep 1
+sbatch --array=53 qsmf_test_1.slurm
+sleep 1
+sbatch --array=54 qsmf_test_1.slurm
+sleep 1
+sbatch --array=56 qsmf_test_1.slurm
+sleep 1
+sbatch --array=57 qsmf_test_1.slurm
+sleep 1
+sbatch --array=58 qsmf_test_1.slurm
+sleep 1
+sbatch --array=59 qsmf_test_1.slurm
+sleep 1
+sbatch --array=60 qsmf_test_1.slurm
+sleep 1
+sbatch --array=61 qsmf_test_1.slurm
+sleep 1
+sbatch --array=62 qsmf_test_1.slurm
+sleep 1
+sbatch --array=63 qsmf_test_1.slurm
+sleep 1
+sbatch --array=64 qsmf_test_1.slurm
+sleep 1
+sbatch --array=66 qsmf_test_1.slurm
+sleep 1
+sbatch --array=68 qsmf_test_1.slurm
+sleep 1
+sbatch --array=69 qsmf_test_1.slurm
+sleep 1
+sbatch --array=70 qsmf_test_1.slurm
+sleep 1
+sbatch --array=71 qsmf_test_1.slurm
+sleep 1
+sbatch --array=72 qsmf_test_1.slurm
+sleep 1
+sbatch --array=73 qsmf_test_1.slurm
+sleep 1
+sbatch --array=74 qsmf_test_1.slurm
+sleep 1
+sbatch --array=75 qsmf_test_1.slurm
+sleep 1
+sbatch --array=76 qsmf_test_1.slurm
+sleep 1
+sbatch --array=77 qsmf_test_1.slurm
+sleep 1
+sbatch --array=78 qsmf_test_1.slurm
+sleep 1
+sbatch --array=79 qsmf_test_1.slurm
+sleep 1
+sbatch --array=80 qsmf_test_1.slurm
+sleep 1
+sbatch --array=81 qsmf_test_1.slurm
+sleep 1
+sbatch --array=82 qsmf_test_1.slurm
+sleep 1
+sbatch --array=83 qsmf_test_1.slurm
+sleep 1
+sbatch --array=85 qsmf_test_1.slurm
+sleep 1
+sbatch --array=87 qsmf_test_1.slurm
+sleep 1
+sbatch --array=88 qsmf_test_1.slurm
+sleep 1
+sbatch --array=89 qsmf_test_1.slurm
+sleep 1
+sbatch --array=90 qsmf_test_1.slurm
+sleep 1
+sbatch --array=91 qsmf_test_1.slurm
+sleep 1
+sbatch --array=92 qsmf_test_1.slurm
+sleep 1
+sbatch --array=93 qsmf_test_1.slurm
+sleep 1
+sbatch --array=95 qsmf_test_1.slurm
+sleep 1
+sbatch --array=96 qsmf_test_1.slurm
+sleep 1
+sbatch --array=97 qsmf_test_1.slurm
+sleep 1
+sbatch --array=98 qsmf_test_1.slurm
+sleep 1
+sbatch --array=99 qsmf_test_1.slurm
+sleep 1
+sbatch --array=101 qsmf_test_1.slurm
+sleep 1
+sbatch --array=102 qsmf_test_1.slurm
+sleep 1
+sbatch --array=103 qsmf_test_1.slurm
+sleep 1
+sbatch --array=104 qsmf_test_1.slurm
+sleep 1
+sbatch --array=105 qsmf_test_1.slurm
+sleep 1
+sbatch --array=106 qsmf_test_1.slurm
+sleep 1
+sbatch --array=107 qsmf_test_1.slurm
+sleep 1
+sbatch --array=108 qsmf_test_1.slurm
+sleep 1
+sbatch --array=109 qsmf_test_1.slurm
+sleep 1
+sbatch --array=110 qsmf_test_1.slurm
+sleep 1
+sbatch --array=111 qsmf_test_1.slurm
+sleep 1
+sbatch --array=112 qsmf_test_1.slurm
+sleep 1
+sbatch --array=114 qsmf_test_1.slurm
+sleep 1
+sbatch --array=115 qsmf_test_1.slurm
+sleep 1
+sbatch --array=116 qsmf_test_1.slurm
+sleep 1
+sbatch --array=117 qsmf_test_1.slurm
+sleep 1
+sbatch --array=118 qsmf_test_1.slurm
+sleep 1
+sbatch --array=119 qsmf_test_1.slurm
+sleep 1
+sbatch --array=120 qsmf_test_1.slurm
+sleep 1
+sbatch --array=122 qsmf_test_1.slurm
+sleep 1
+sbatch --array=123 qsmf_test_1.slurm
+sleep 1
+sbatch --array=124 qsmf_test_1.slurm
+sleep 1
+sbatch --array=126 qsmf_test_1.slurm
+sleep 1
+sbatch --array=127 qsmf_test_1.slurm
+sleep 1
+sbatch --array=128 qsmf_test_1.slurm
+sleep 1
+sbatch --array=129 qsmf_test_1.slurm
+sleep 1
+sbatch --array=130 qsmf_test_1.slurm
+sleep 1
+sbatch --array=133 qsmf_test_1.slurm
+sleep 1
+sbatch --array=134 qsmf_test_1.slurm
+sleep 1
+sbatch --array=135 qsmf_test_1.slurm
+sleep 1
+sbatch --array=136 qsmf_test_1.slurm
+sleep 1
+sbatch --array=137 qsmf_test_1.slurm
+sleep 1
+sbatch --array=138 qsmf_test_1.slurm
+sleep 1
+sbatch --array=139 qsmf_test_1.slurm
+sleep 1
+sbatch --array=140 qsmf_test_1.slurm
+sleep 1
+sbatch --array=141 qsmf_test_1.slurm
+sleep 1
+sbatch --array=142 qsmf_test_1.slurm
+sleep 1
+sbatch --array=143 qsmf_test_1.slurm
+sleep 1
+sbatch --array=144 qsmf_test_1.slurm
+sleep 1
+sbatch --array=145 qsmf_test_1.slurm
+sleep 1
+sbatch --array=146 qsmf_test_1.slurm
+sleep 1
+sbatch --array=147 qsmf_test_1.slurm
+sleep 1
+sbatch --array=148 qsmf_test_1.slurm
+sleep 1
+sbatch --array=149 qsmf_test_1.slurm
+sleep 1
+sbatch --array=150 qsmf_test_1.slurm
+sleep 1
+sbatch --array=151 qsmf_test_1.slurm
+sleep 1
+sbatch --array=152 qsmf_test_1.slurm
+sleep 1
+sbatch --array=153 qsmf_test_1.slurm
+sleep 1
+sbatch --array=154 qsmf_test_1.slurm
+sleep 1
+sbatch --array=155 qsmf_test_1.slurm
+sleep 1
+sbatch --array=156 qsmf_test_1.slurm
+sleep 1
+sbatch --array=157 qsmf_test_1.slurm
+sleep 1
+sbatch --array=158 qsmf_test_1.slurm
+sleep 1
+sbatch --array=159 qsmf_test_1.slurm
+sleep 1
+sbatch --array=160 qsmf_test_1.slurm
+sleep 1
+sbatch --array=161 qsmf_test_1.slurm
+sleep 1
+sbatch --array=162 qsmf_test_1.slurm
+sleep 1
+sbatch --array=163 qsmf_test_1.slurm
+sleep 1
+sbatch --array=164 qsmf_test_1.slurm
+sleep 1
+sbatch --array=165 qsmf_test_1.slurm
+sleep 1
+sbatch --array=166 qsmf_test_1.slurm
+sleep 1
+sbatch --array=167 qsmf_test_1.slurm
+sleep 1
+sbatch --array=168 qsmf_test_1.slurm
+sleep 1
+sbatch --array=169 qsmf_test_1.slurm
+sleep 1
+sbatch --array=170 qsmf_test_1.slurm
+sleep 1
+sbatch --array=171 qsmf_test_1.slurm
+sleep 1
+sbatch --array=172 qsmf_test_1.slurm
+sleep 1
+sbatch --array=173 qsmf_test_1.slurm
+sleep 1
+sbatch --array=174 qsmf_test_1.slurm
+sleep 1
+sbatch --array=175 qsmf_test_1.slurm
+sleep 1
+sbatch --array=176 qsmf_test_1.slurm
+sleep 1
+sbatch --array=177 qsmf_test_1.slurm
+sleep 1
+sbatch --array=178 qsmf_test_1.slurm
+sleep 1
+sbatch --array=179 qsmf_test_1.slurm
+sleep 1
+sbatch --array=180 qsmf_test_1.slurm
+sleep 1
+sbatch --array=181 qsmf_test_1.slurm
+sleep 1
+sbatch --array=182 qsmf_test_1.slurm
+sleep 1
+sbatch --array=183 qsmf_test_1.slurm
+sleep 1
+sbatch --array=184 qsmf_test_1.slurm
+sleep 1
+sbatch --array=185 qsmf_test_1.slurm
+sleep 1
+sbatch --array=186 qsmf_test_1.slurm
+sleep 1
+sbatch --array=187 qsmf_test_1.slurm
+sleep 1
+sbatch --array=188 qsmf_test_1.slurm
+sleep 1
+sbatch --array=189 qsmf_test_1.slurm
+sleep 1
+sbatch --array=190 qsmf_test_1.slurm
+sleep 1
+sbatch --array=191 qsmf_test_1.slurm
+sleep 1
+sbatch --array=192 qsmf_test_1.slurm
+sleep 1
+sbatch --array=193 qsmf_test_1.slurm
+sleep 1
+sbatch --array=194 qsmf_test_1.slurm
+sleep 1
+sbatch --array=195 qsmf_test_1.slurm
+sleep 1
+sbatch --array=196 qsmf_test_1.slurm
+sleep 1
+sbatch --array=197 qsmf_test_1.slurm
+sleep 1
+sbatch --array=198 qsmf_test_1.slurm
+sleep 1
+sbatch --array=199 qsmf_test_1.slurm
+sleep 1
+sbatch --array=200 qsmf_test_1.slurm
+sleep 1
+sbatch --array=201 qsmf_test_1.slurm
+sleep 1
+sbatch --array=202 qsmf_test_1.slurm
+sleep 1
+sbatch --array=203 qsmf_test_1.slurm
+sleep 1
+sbatch --array=204 qsmf_test_1.slurm
+sleep 1
+sbatch --array=205 qsmf_test_1.slurm
+sleep 1
+sbatch --array=206 qsmf_test_1.slurm
+sleep 1
+sbatch --array=207 qsmf_test_1.slurm
+sleep 1
+sbatch --array=208 qsmf_test_1.slurm
+sleep 1
+sbatch --array=209 qsmf_test_1.slurm
+sleep 1
+sbatch --array=210 qsmf_test_1.slurm
+sleep 1
+sbatch --array=211 qsmf_test_1.slurm
+sleep 1
+sbatch --array=212 qsmf_test_1.slurm
+sleep 1
+sbatch --array=213 qsmf_test_1.slurm
+sleep 1
+sbatch --array=214 qsmf_test_1.slurm
+sleep 1
+sbatch --array=215 qsmf_test_1.slurm
+sleep 1
+sbatch --array=216 qsmf_test_1.slurm
+sleep 1
+sbatch --array=217 qsmf_test_1.slurm
+sleep 1
+sbatch --array=218 qsmf_test_1.slurm
+sleep 1
+sbatch --array=219 qsmf_test_1.slurm
+sleep 1
+sbatch --array=220 qsmf_test_1.slurm
+sleep 1
+sbatch --array=221 qsmf_test_1.slurm
+sleep 1
+sbatch --array=222 qsmf_test_1.slurm
+sleep 1
+sbatch --array=223 qsmf_test_1.slurm
+sleep 1
+sbatch --array=224 qsmf_test_1.slurm
+sleep 1
+sbatch --array=225 qsmf_test_1.slurm
+sleep 1
+sbatch --array=226 qsmf_test_1.slurm
+sleep 1
+sbatch --array=227 qsmf_test_1.slurm
+sleep 1
+sbatch --array=228 qsmf_test_1.slurm
+sleep 1
+sbatch --array=229 qsmf_test_1.slurm
+sleep 1
+sbatch --array=230 qsmf_test_1.slurm
+sleep 1
+sbatch --array=231 qsmf_test_1.slurm
+sleep 1
+sbatch --array=232 qsmf_test_1.slurm
+sleep 1
+sbatch --array=233 qsmf_test_1.slurm
+sleep 1
+sbatch --array=234 qsmf_test_1.slurm
+sleep 1
+sbatch --array=235 qsmf_test_1.slurm
+sleep 1
+sbatch --array=236 qsmf_test_1.slurm
+sleep 1
+sbatch --array=237 qsmf_test_1.slurm
+sleep 1
+sbatch --array=238 qsmf_test_1.slurm
+sleep 1
+sbatch --array=239 qsmf_test_1.slurm
+sleep 1
+sbatch --array=240 qsmf_test_1.slurm
+sleep 1
+sbatch --array=241 qsmf_test_1.slurm
+sleep 1
+sbatch --array=242 qsmf_test_1.slurm
+sleep 1
+sbatch --array=243 qsmf_test_1.slurm
+sleep 1
+sbatch --array=244 qsmf_test_1.slurm
+sleep 1
+sbatch --array=245 qsmf_test_1.slurm
+sleep 1
+sbatch --array=246 qsmf_test_1.slurm
+sleep 1
+sbatch --array=247 qsmf_test_1.slurm
+sleep 1
+sbatch --array=248 qsmf_test_1.slurm
+sleep 1
+sbatch --array=249 qsmf_test_1.slurm
+sleep 1
+sbatch --array=250 qsmf_test_1.slurm
+sleep 1
+sbatch --array=251 qsmf_test_1.slurm
+sleep 1
+sbatch --array=252 qsmf_test_1.slurm
+sleep 1
+echo Done
