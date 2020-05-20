94ff8410c6534868bb202c2ef48810458052f333
Alex Salois <asalois.scholar@gmail.com>
2020-05-20 09:35:00 -0600

    data processed and ready to run more sims

--- series_02/mpi_0/span_025/relaunch.sh
+++ series_02/mpi_0/span_025/relaunch.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
 # to relaunch jobs
+sbatch --array=5 qsmf_test_1.slurm
+sleep 1
+sbatch --array=6 qsmf_test_1.slurm
+sleep 1
+sbatch --array=7 qsmf_test_1.slurm
+sleep 1
+sbatch --array=8 qsmf_test_1.slurm
+sleep 1
+sbatch --array=9 qsmf_test_1.slurm
+sleep 1
+sbatch --array=11 qsmf_test_1.slurm
+sleep 1
+sbatch --array=12 qsmf_test_1.slurm
+sleep 1
+sbatch --array=13 qsmf_test_1.slurm
+sleep 1
 sbatch --array=14 qsmf_test_1.slurm
+sleep 1
+sbatch --array=15 qsmf_test_1.slurm
+sleep 1
+sbatch --array=16 qsmf_test_1.slurm
+sleep 1
+sbatch --array=17 qsmf_test_1.slurm
 sleep 1
 sbatch --array=18 qsmf_test_1.slurm
 sleep 1
 sbatch --array=19 qsmf_test_1.slurm
 sleep 1
+sbatch --array=20 qsmf_test_1.slurm
+sleep 1
+sbatch --array=22 qsmf_test_1.slurm
+sleep 1
 sbatch --array=23 qsmf_test_1.slurm
 sleep 1
 sbatch --array=24 qsmf_test_1.slurm
 sleep 1
+sbatch --array=25 qsmf_test_1.slurm
+sleep 1
 sbatch --array=26 qsmf_test_1.slurm
 sleep 1
+sbatch --array=27 qsmf_test_1.slurm
+sleep 1
 sbatch --array=28 qsmf_test_1.slurm
+sleep 1
+sbatch --array=29 qsmf_test_1.slurm
+sleep 1
+sbatch --array=30 qsmf_test_1.slurm
 sleep 1
 sbatch --array=31 qsmf_test_1.slurm
 sleep 1
 sbatch --array=32 qsmf_test_1.slurm
 sleep 1
 sbatch --array=33 qsmf_test_1.slurm
+sleep 1
+sbatch --array=34 qsmf_test_1.slurm
 sleep 1
 sbatch --array=35 qsmf_test_1.slurm
 sleep 1
@@ -30,11 +66,29 @@ sbatch --array=38 qsmf_test_1.slurm
 sleep 1
 sbatch --array=39 qsmf_test_1.slurm
 sleep 1
+sbatch --array=40 qsmf_test_1.slurm
+sleep 1
+sbatch --array=41 qsmf_test_1.slurm
+sleep 1
+sbatch --array=42 qsmf_test_1.slurm
+sleep 1
 sbatch --array=43 qsmf_test_1.slurm
+sleep 1
+sbatch --array=44 qsmf_test_1.slurm
 sleep 1
 sbatch --array=45 qsmf_test_1.slurm
 sleep 1
+sbatch --array=46 qsmf_test_1.slurm
+sleep 1
+sbatch --array=47 qsmf_test_1.slurm
+sleep 1
+sbatch --array=48 qsmf_test_1.slurm
+sleep 1
+sbatch --array=49 qsmf_test_1.slurm
+sleep 1
 sbatch --array=50 qsmf_test_1.slurm
+sleep 1
+sbatch --array=51 qsmf_test_1.slurm
 sleep 1
 sbatch --array=52 qsmf_test_1.slurm
 sleep 1
@@ -42,13 +96,51 @@ sbatch --array=53 qsmf_test_1.slurm
 sleep 1
 sbatch --array=54 qsmf_test_1.slurm
 sleep 1
+sbatch --array=55 qsmf_test_1.slurm
+sleep 1
 sbatch --array=56 qsmf_test_1.slurm
+sleep 1
+sbatch --array=57 qsmf_test_1.slurm
+sleep 1
+sbatch --array=58 qsmf_test_1.slurm
+sleep 1
+sbatch --array=59 qsmf_test_1.slurm
+sleep 1
+sbatch --array=60 qsmf_test_1.slurm
+sleep 1
+sbatch --array=61 qsmf_test_1.slurm
+sleep 1
+sbatch --array=62 qsmf_test_1.slurm
+sleep 1
+sbatch --array=63 qsmf_test_1.slurm
+sleep 1
+sbatch --array=64 qsmf_test_1.slurm
+sleep 1
+sbatch --array=65 qsmf_test_1.slurm
+sleep 1
+sbatch --array=66 qsmf_test_1.slurm
+sleep 1
+sbatch --array=67 qsmf_test_1.slurm
+sleep 1
+sbatch --array=68 qsmf_test_1.slurm
 sleep 1
 sbatch --array=69 qsmf_test_1.slurm
 sleep 1
 sbatch --array=70 qsmf_test_1.slurm
 sleep 1
 sbatch --array=71 qsmf_test_1.slurm
+sleep 1
+sbatch --array=72 qsmf_test_1.slurm
+sleep 1
+sbatch --array=73 qsmf_test_1.slurm
+sleep 1
+sbatch --array=74 qsmf_test_1.slurm
+sleep 1
+sbatch --array=75 qsmf_test_1.slurm
+sleep 1
+sbatch --array=76 qsmf_test_1.slurm
+sleep 1
+sbatch --array=77 qsmf_test_1.slurm
 sleep 1
 sbatch --array=78 qsmf_test_1.slurm
 sleep 1
@@ -62,7 +154,11 @@ sbatch --array=82 qsmf_test_1.slurm
 sleep 1
 sbatch --array=83 qsmf_test_1.slurm
 sleep 1
+sbatch --array=84 qsmf_test_1.slurm
+sleep 1
 sbatch --array=85 qsmf_test_1.slurm
+sleep 1
+sbatch --array=86 qsmf_test_1.slurm
 sleep 1
 sbatch --array=87 qsmf_test_1.slurm
 sleep 1
@@ -78,17 +174,25 @@ sbatch --array=92 qsmf_test_1.slurm
 sleep 1
 sbatch --array=93 qsmf_test_1.slurm
 sleep 1
-sbatch --array=95 qsmf_test_1.slurm
+sbatch --array=94 qsmf_test_1.slurm
 sleep 1
-sbatch --array=96 qsmf_test_1.slurm
+sbatch --array=95 qsmf_test_1.slurm
 sleep 1
 sbatch --array=97 qsmf_test_1.slurm
 sleep 1
-sbatch --array=98 qsmf_test_1.slurm
+sbatch --array=99 qsmf_test_1.slurm
 sleep 1
 sbatch --array=101 qsmf_test_1.slurm
 sleep 1
+sbatch --array=102 qsmf_test_1.slurm
+sleep 1
+sbatch --array=103 qsmf_test_1.slurm
+sleep 1
+sbatch --array=104 qsmf_test_1.slurm
+sleep 1
 sbatch --array=105 qsmf_test_1.slurm
+sleep 1
+sbatch --array=106 qsmf_test_1.slurm
 sleep 1
 sbatch --array=107 qsmf_test_1.slurm
 sleep 1
@@ -101,6 +205,8 @@ sleep 1
 sbatch --array=111 qsmf_test_1.slurm
 sleep 1
 sbatch --array=112 qsmf_test_1.slurm
+sleep 1
+sbatch --array=113 qsmf_test_1.slurm
 sleep 1
 sbatch --array=114 qsmf_test_1.slurm
 sleep 1
@@ -116,11 +222,15 @@ sbatch --array=119 qsmf_test_1.slurm
 sleep 1
 sbatch --array=120 qsmf_test_1.slurm
 sleep 1
+sbatch --array=121 qsmf_test_1.slurm
+sleep 1
 sbatch --array=122 qsmf_test_1.slurm
 sleep 1
 sbatch --array=123 qsmf_test_1.slurm
 sleep 1
 sbatch --array=124 qsmf_test_1.slurm
+sleep 1
+sbatch --array=125 qsmf_test_1.slurm
 sleep 1
 sbatch --array=126 qsmf_test_1.slurm
 sleep 1
@@ -131,5 +241,9 @@ sleep 1
 sbatch --array=129 qsmf_test_1.slurm
 sleep 1
 sbatch --array=130 qsmf_test_1.slurm
+sleep 1
+sbatch --array=131 qsmf_test_1.slurm
+sleep 1
+sbatch --array=132 qsmf_test_1.slurm
 sleep 1
 echo Done
