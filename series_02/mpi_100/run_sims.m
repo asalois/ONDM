d0b5deda69f4d284bdac73e5d9149c20bc9ac88d
Alex Salois <asalois.scholar@gmail.com>
2020-05-31 13:52:56 -0600

    run sim on laptop

--- series_02/mpi_100/run_sims.m
+++ series_02/mpi_100/run_sims.m
@@ -4,7 +4,7 @@ step  = [1, 2, 2.5, 3, 4, 5, 6, 7.5, 8, 10, 10]; % step length [km]
 laser_power = [[-9:1:2]', [-9:1:2]', [-9:1:2]', [-8:1:3]', [-8:1:3]', [-7:1:4]', [-7:1:4]', [-6:1:5]', [-6:1:5]', [-3:1:8]', [-1:1:10]']; % Laser power [dBm]
 %addpath('/mnt/lustrefs/scratch/v16b915/series_02/sim_files')
 addpath('/home/alex/Documents/ONDM/series_02/sim_files')
-comp = 0; % percentage of MPI compensation.  0 - full MPI, 100 - MPI fully compensated
+comp = 100; % percentage of MPI compensation.  0 - full MPI, 100 - MPI fully compensated
 alpha1 = 0.16; % QSM fiber attenuation [dB/km]
 alpha2 = 0.158; % EX3000 fiber attenuation [dB/km]
 aeff1 = 250; % QSM fiber effective area  [um^2]
