9cc6b95a9c0022cff61875228b1e56d4f77d04a1
Alex Salois <asalois.scholar@gmail.com>
2020-06-01 22:48:03 -0600

    1st batch of gpus runs

--- gpu_test/test_sim.sh
+++ gpu_test/test_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # find the files that outputted then make a script to relaunch jobs
-for i in {924..930}
+for i in {931..948}
 do
 	echo "$i"
 	matlab -nodesktop -nodisplay -nosplash -r "run_sims($i); exit;"
