9b79a4540e25210266caac43762fd0e849ec70cd
Alex Salois <asalois.scholar@gmail.com>
2020-06-04 18:59:23 -0600

    desktop changes

--- fftw_test/fftw_test.m
+++ fftw_test/fftw_test.m
@@ -3,12 +3,25 @@ clc
 clear all
 runs = 50;
 power = 13;
-times = zeros(2,power*runs);
-
+times = zeros(5,power*runs);
+for j = 1:5
 for k = 1:power
     array_size = 2^k;
     fftw('dwisdom', []);
-    fftw('planner','patient');
+    switch j
+	case 1   
+    		fftw('planner','estimate');
+    	case 2   
+    		fftw('planner','measure');
+	case 3   
+    		fftw('planner','patient');
+	case 4   
+    		fftw('planner','exhaustive');
+	case 5
+		fftw('planner', 'hybrid');	
+	otherwise
+		disp('not it')
+    end
     for i = ((k-1)*runs + 1):k*runs
         % cpu fft test
         tic;
@@ -17,15 +30,16 @@ for k = 1:power
         inv = ifft(cpu);
         time_cpu = toc;
         
-        times(1,i) = time_cpu;
-        times(2,i) = array_size;
+        times(j,i) = time_cpu;
     end
 end
 sum(times(1,:))
+end
+x = 1:power*runs;
 figure()
-fig = plot(times(1,:));
+fig = plot(x, times(1,:), x, times(2,:), x, times(3,:), x, times(5,:));
 title('Time (s)');
 xlabel('run #');
 ylabel('Time (Seconds)');
-%legend({'cpu', 'gpu'}, 'Location', 'northwest');
+legend({'estimate', 'measure', 'patient', 'exhaustive',' hybrid'},'Location', 'northwest');
 saveas(gcf, 'time.png');
