ae77aa726189bcb3f53115c7eecdb070cf144ba0
Alex Salois <asalois.scholar@gmail.com>
2020-06-10 21:48:03 -0600

    parfor test

--- gpu_test/test_sim.sh
+++ gpu_test/test_sim.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # run matlab sims 
-for i in {1143..1180}
-do
-	echo "$i"
-	matlab -nodesktop -nodisplay -nosplash -r "run_sims($i); exit;" >> out.txt
-	echo "$i done"
-done
+i=1300
+echo "$i"
+matlab -nodesktop -nodisplay -nosplash -r "run_sims($i); exit;" >> out.txt
+echo "$i done"
