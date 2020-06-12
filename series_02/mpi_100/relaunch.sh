3348cfbe369036b42e8d679d477a3f98254fad1e
Alex Salois <asalois.scholar@gmail.com>
2020-06-12 14:40:39 -0600

    the final batch

--- series_02/mpi_100/relaunch.sh
+++ series_02/mpi_100/relaunch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # to relaunch jobs
-sbatch --array=0%20 qsmf.slurm
+sbatch --array=0%0 qsmf.slurm
+sleep 1
+sbatch --array=53 qsmf_2.slurm
+sleep 1
+sbatch --array=206,303,363,436,521 qsmf_2.slurm
 sleep 1
 echo Done
