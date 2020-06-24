431df7a329e3704646a67682631f99c15029b6eb
Alex Salois <asalois.scholar@gmail.com>
2020-06-24 15:07:58 -0600

    added number of runs parfor

--- new_stuff/mmf_sim.m
+++ new_stuff/mmf_sim.m
@@ -1,7 +1,7 @@
 clc;
 close all;
 clear all;
-Fs = 256*10^3;% Sampling frequency
+Fs = 256*10^1;% Sampling frequency
 t = -1:1/Fs:1;      % Time vector 
 L = length(t);      % Signal length
 Ak = 1;
@@ -11,34 +11,38 @@ X = Ak*exp(-t.^2/(2*To^2)); % Gausian Pulse
 n = 2^nextpow2(L);  % for better fft perf
 Y = fft(X,n);   % the fft of the pulse
 Z = [Y;Y];
-Z = Z';
 num_modes = 2;
-inl = [To To];
-y = zeros(num_modes,n);
+num_runs = 100;
+inl = [1 1];
+y = zeros(num_runs, num_modes,n);
 
-for i = 1:length(Y) 
-    w = 2*pi/i;
-    l = lamda(inl, w);
-    u = rand(num_modes);
-    m = l*u;
-    y(:, i) = Z(i,:)*m;
-    y(:, i) = ifft(y(:, i));
+tic
+parfor j = 1:num_runs
+    for i = 1:n
+        w = 2*pi/i;
+        l = lamda(inl, w);
+        u = rand(num_modes);
+        m = l*u;
+        y(j,:,i) = m*Z(:,i);
+    end
 end
+toc
 
+% % plot
+% figure()
+% plot(t,X(1:length(t)) )
+% title('Gaussian Pulse in Time Domain')
+% xlabel('Time (t)')
+% ylabel('X(t)')
+%y= y';
+x = ifft(y,[],2);
+%x = x';
 
-
-% plot
-figure()
-plot(t,X(1:length(t)) )
-title('Gaussian Pulse in Time Domain')
-xlabel('Time (t)')
-ylabel('X(t)')
-
-
+%%
 figure()
 hold on
-for i = 1:num_modes
-    plot(t,abs(y(i,(1:length(t)))) )
+for i = 1:num_runs
+    plot(t,abs(x(i,(1:length(t)))) )
 end
 title('Gaussian Pulse in Time Domain')
 xlabel('Time (t)')
