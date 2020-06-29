d18239d842e7e940d8bd0625d5a5c64b78d0c408
Alex Salois <asalois.scholar@gmail.com>
2020-06-29 15:55:17 -0600

    something looks better

--- new_stuff/plot_it.m
+++ new_stuff/plot_it.m
@@ -14,5 +14,20 @@ function plot_it(t, x, To, k)
     name =  sprintf(formatSpec,k);
     hold off
     saveas(gcf,name,'png');
+    
+    figure()
+    hold on
+    for i = 1:size(x,1)
+        plot(t,angle(x(i,(1:length(t)))) )
+    end
+    formatSpec = 'Gaussian Pulse w/ To = %5.4f in Time Domain\n';
+    ttl = sprintf(formatSpec,To);
+    title(ttl);
+    xlabel('Time (t)')
+    ylabel('X(t)')
+    formatSpec = 'toangle_%d';
+    name =  sprintf(formatSpec,k);
+    hold off
+    saveas(gcf,name,'png');
 end
 
