42f4a36c6b13be6f22919da4394c0ff804d07973
Alex Salois <asalois.scholar@gmail.com>
2020-07-24 14:37:40 -0600

    include as backup

--- new_stuff/mmf_MD.m
+++ new_stuff/mmf_MD.m
@@ -1,4 +1,4 @@
-function [y] = mmf_MD(Y, delay,f)
+function [y] = mmf_MD(Y, delay, f)
     % Multimode fiber simultion of Modal Dispersion using matrix concatantion
     ees = exp(-j*2*pi*delay.*f);
     y = Y.*ees;
