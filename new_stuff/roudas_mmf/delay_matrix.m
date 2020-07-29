7b7787eb1bfadb902aa5f453c8b4308210e35dca
Alex Salois <asalois.scholar@gmail.com>
2020-07-29 13:00:28 -0600

    get the out of the pulse

--- new_stuff/roudas_mmf/delay_matrix.m
+++ new_stuff/roudas_mmf/delay_matrix.m
@@ -1,6 +1,7 @@
 function d = delay_matrix(frequency, dgd)
+% This creates a delay matrix that is based on the inputs frequcny and dgd
     omega = 2 * pi * frequency;
     phase_shift = omega * dgd;
     v = exp([-j*phase_shift/2 j*phase_shift/2]);
-    d = diag(v)
+    d = diag(v);
 end
--- new_stuff/roudas_mmf/new_mmf_sim_IR.m
+++ new_stuff/roudas_mmf/new_mmf_sim_IR.m
@@ -42,11 +42,11 @@ g = exp(-t.^2/(2*To^2)); % Gaussian vector
 
 %% Plot a Gaussian pulse
 
-figure()
-plot(t/To,g), 
-legend('Transmitted pulse')
-xlabel('Time (To)'), ylabel('s (t)')
-title('Transmitted Gaussian pulse')
+% figure()
+% plot(t/To,g), 
+% legend('Transmitted pulse')
+% xlabel('Time (To)'), ylabel('s (t)')
+% title('Transmitted Gaussian pulse')
 
 %% Gaussian pulse spectrum
 
@@ -56,11 +56,11 @@ f = ((-nfft/2+1/2):(nfft/2-1/2))*df; % zero-centered frequency range
 
 %% Plot a Gaussian pulse spectrum
 
-figure()
-plot(f/Fs,abs(G)), 
-legend('Transmitted pulse spectrum')
-xlabel('Frequency (fs)'), ylabel('G (f)')
-title('Transmitted Gaussian pulse')
+% figure()
+% plot(f/Fs,abs(G)), 
+% legend('Transmitted pulse spectrum')
+% xlabel('Frequency (fs)'), ylabel('G (f)')
+% title('Transmitted Gaussian pulse')
 
 
 %% Transfer matrix of the SMF
@@ -70,7 +70,7 @@ title('Transmitted Gaussian pulse')
 rng(0,'twister'); % Initialize the random number generator
 theta = 2*pi*rand(num_fiber_sections+1,1); %Generate random phases
 
-%We will create nfft 2x2 random matrices
+% We will create nfft 2x2 random matrices
 
 output_matrix = zeros(2,2,nfft); % Fiber transfer matrix initialization
 
@@ -90,6 +90,33 @@ end
 % You can multiply the transfer matrix at each frequency with the input
 % Jones vector. This will give us an output Jones vector per frequency.
 % Each Jones vector is scaled by the pulse spectrum G(f(k))
+clc
+out_pulse = zeros(2,256);
+for k = 1:nfft
+    m = launch_jones_vector .* G(k);
+    m = output_matrix(:,:,k) * m;
+    out_pulse(:,k) = m;
+end
+pulse = out_pulse(1,:) + out_pulse(2,:);
 
+% figure()
+% hold on
+% plot(f/Fs,abs(out_pulse(1,:)))
+% plot(f/Fs,abs(out_pulse(2,:)))
+% plot(f/Fs,abs(pulse))
+% xlabel('Frequency (fs)'), ylabel('G (f)')
+% title('Transmitted Gaussian pulse')
+% hold off
 
+p0 = ifft(pulse);
+p1 = ifft(out_pulse(1,:));
+p2 = ifft(out_pulse(2,:));
+p3 = p1 + p2;
+figure()
+hold on
+plot(abs(p0))
+plot(abs(p1))
+plot(abs(p2))
+plot(abs(p3))
+hold off
 
