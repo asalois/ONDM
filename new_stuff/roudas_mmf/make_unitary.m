7877bc6dc748a6a52e8a2b8cc9476e03ee78948a
Alex Salois <asalois.scholar@gmail.com>
2020-07-31 12:29:26 -0600

    added function that makes a random unitary matrix of NxN

---
+++ new_stuff/roudas_mmf/make_unitary.m
@@ -0,0 +1,7 @@
+function U = make_unitary(N)
+% Make a Unitary Matrix of NxN using Ozol's Method
+rr = normrnd(0,1,N,N); % make a NxN matrix of random numbers from normal distribution
+ri = normrnd(0,1,N,N);
+U = rr + ri*1i; % make complex
+end
+
