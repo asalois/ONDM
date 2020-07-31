36607d067f53ae256cf0469692bc20d0adc5bbef
Alex Salois <asalois.scholar@gmail.com>
2020-07-31 12:34:54 -0600

    move and delete

--- new_stuff/lambda.m
+++
@@ -1,8 +0,0 @@
-function [out] = lambda(in1, w)
-% Makes a matrix length(inl) x length(inl) with the diagonals being inl
-n = length(in1);
-I = eye(n);
-ex = exp(-j*w.*in1);
-out =  I.*ex;
-end
-
--- new_stuff/make_unitary.m
+++
@@ -1,7 +0,0 @@
-function [m] = make_unitary()
-% Makes 2x2 special unitary matrix that is complex
-%   Detailed explanation goes here
-    phy = rand(1) + rand(1)*i;
-    m = [cos(phy) sin(phy); -sin(phy) cos(phy)];
-end
-
