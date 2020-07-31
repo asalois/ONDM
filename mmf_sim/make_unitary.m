02b3a0517a8a75e35c321d84e0209fc5b1a0cd4a
Alex Salois <asalois.scholar@gmail.com>
2020-07-31 12:57:27 -0600

    added othro function to finish Ozol's method

--- mmf_sim/make_unitary.m
+++ mmf_sim/make_unitary.m
@@ -3,5 +3,6 @@ function U = make_unitary(N)
     rr = normrnd(0,1,N,N); % make a NxN matrix of random numbers from normal distribution
     ri = normrnd(0,1,N,N);
     U = rr + ri*1i; % make complex
+    U = orth(U); % orthangonalize
 end
 
