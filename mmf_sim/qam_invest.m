3def935448da01eb0c3dc593a71cd14353931276
Alex Salois <asalois.scholar@gmail.com>
2020-08-06 10:14:38 -0600

    plots impulse response of filter

--- mmf_sim/qam_invest.m
+++ mmf_sim/qam_invest.m
@@ -4,6 +4,12 @@
 % Electrical & Computer Engineering Department
 % Created by Alexander Salois
 
+% message_size = 8;
+% m = 2^4;
+% data_in = randi(15,1,message_size)
+% modded = qammod(data_in,m)
+% rec = qamdemod(modded,m)
+
 %% Preliminary commands 
 
 close all;
@@ -17,10 +23,18 @@ n = 20000; % Transmitted bits
 nSamp = 4; % Samples per symbol
 EbNo = 10; % Eb/No (dB)
 
-%%
+%% Filter Paramters
+span = 10; % Filter span in symbols
+rolloff = 0.25; % Rolloff factor
 
-message_size = 8;
-m = 2^4;
-data_in = randi(15,1,message_size)
-modded = qammod(data_in,m)
-rec = qamdemod(modded,m)
+
+%% Create the raised cosine transmit and receive filters using the previously defined parameters.
+txfilter = comm.RaisedCosineTransmitFilter('RolloffFactor',rolloff, ...
+    'FilterSpanInSymbols',span,'OutputSamplesPerSymbol',nSamp);
+
+rxfilter = comm.RaisedCosineReceiveFilter('RolloffFactor',rolloff, ...
+    'FilterSpanInSymbols',span,'InputSamplesPerSymbol',nSamp, ...
+    'DecimationFactor',nSamp);
+
+%% Plot the impulse response of hTxFilter.
+fvtool(txfilter,'impulse')
