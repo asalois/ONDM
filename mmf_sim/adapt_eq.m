8b25785a9bc0fd8998835e9dc7216f8ae0f83d15
Alex Salois <asalois.scholar@gmail.com>
2020-10-12 16:27:59 -0600

    changed some params to learn more

--- mmf_sim/adapt_eq.m
+++ mmf_sim/adapt_eq.m
@@ -1,16 +1,21 @@
 % Matlab implentmation of adaptive equilzers using Com toolbox
 
+
 %% system setup
+clc;
+clear;
+close all;
+
 M = 4;
 numTrainSymbols = 200;
-numDataSymbols = 1800;
+numDataSymbols = 2000;
 SNR = 20;
 trainingSymbols = pskmod(randi([0 M-1],numTrainSymbols,1),M,pi/4);
 numPkts = 10;
 lineq = comm.LinearEqualizer('Algorithm','LMS', ...
-    'NumTaps',5,'ReferenceTap',3,'StepSize',0.01);
+    'NumTaps',8,'StepSize',0.01);
 
-%% Train the Equalizer at the Beginning of Each Packet With Reset
+% Train the Equalizer at the Beginning of Each Packet With Reset
 jj = 1;
 figure
 for ii = 1:numPkts
@@ -34,24 +39,24 @@ end
 
 
 %% Train the Equalizer at the Beginning of Each Packet Without Reset
-release(lineq)
-jj = 1;
-figure
-for ii = 1:numPkts
-    b = randi([0 M-1],numDataSymbols,1);
-    dataSym = pskmod(b,M,pi/4);
-    packet = [trainingSymbols;dataSym];
-    channel = 1;
-    rx = awgn(packet*channel,SNR);
-    [~,err] = lineq(rx,trainingSymbols);
-    if (ii ==1 || ii == 2 ||ii == numPkts)
-        subplot(3,1,jj)
-        plot(abs(err))
-        title(['Packet # ',num2str(ii)])
-        xlabel('Symbols')
-        ylabel('Error Magnitude')
-        axis([0,length(packet),0,1])
-        grid on;
-        jj = jj+1;
-    end
-end
+% release(lineq)
+% jj = 1;
+% figure
+% for ii = 1:numPkts
+%     b = randi([0 M-1],numDataSymbols,1);
+%     dataSym = pskmod(b,M,pi/4);
+%     packet = [trainingSymbols;dataSym];
+%     channel = 1;
+%     rx = awgn(packet*channel,SNR);
+%     [~,err] = lineq(rx,trainingSymbols);
+%     if (ii ==1 || ii == 2 ||ii == numPkts)
+%         subplot(3,1,jj)
+%         plot(abs(err))
+%         title(['Packet # ',num2str(ii)])
+%         xlabel('Symbols')
+%         ylabel('Error Magnitude')
+%         axis([0,length(packet),0,1])
+%         grid on;
+%         jj = jj+1;
+%     end
+% end
