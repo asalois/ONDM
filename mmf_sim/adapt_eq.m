1f2e797c9117cfa989111be4870cf66d8127249c
Alexander Salois <asalois.scholar@gmail.com>
2020-10-12 21:23:54 -0600

    added time-varying channels that require periodic equalizer

--- mmf_sim/adapt_eq.m
+++ mmf_sim/adapt_eq.m
@@ -13,11 +13,13 @@ SNR = 20;
 trainingSymbols = pskmod(randi([0 M-1],numTrainSymbols,1),M,pi/4);
 numPkts = 10;
 lineq = comm.LinearEqualizer('Algorithm','LMS', ...
-    'NumTaps',8,'StepSize',0.01);
+    'NumTaps',4,'StepSize',0.01);
 
+%%
 % Train the Equalizer at the Beginning of Each Packet With Reset
 jj = 1;
-figure
+figure()
+title('With Reset')
 for ii = 1:numPkts
     b = randi([0 M-1],numDataSymbols,1);
     dataSym = pskmod(b,M,pi/4);
@@ -38,25 +40,94 @@ for ii = 1:numPkts
 end
 
 
-%% Train the Equalizer at the Beginning of Each Packet Without Reset
-% release(lineq)
-% jj = 1;
-% figure
-% for ii = 1:numPkts
-%     b = randi([0 M-1],numDataSymbols,1);
-%     dataSym = pskmod(b,M,pi/4);
-%     packet = [trainingSymbols;dataSym];
-%     channel = 1;
-%     rx = awgn(packet*channel,SNR);
-%     [~,err] = lineq(rx,trainingSymbols);
-%     if (ii ==1 || ii == 2 ||ii == numPkts)
-%         subplot(3,1,jj)
-%         plot(abs(err))
-%         title(['Packet # ',num2str(ii)])
-%         xlabel('Symbols')
-%         ylabel('Error Magnitude')
-%         axis([0,length(packet),0,1])
-%         grid on;
-%         jj = jj+1;
-%     end
-% end
+% Train the Equalizer at the Beginning of Each Packet Without Reset
+release(lineq)
+jj = 1;
+figure()
+title('Without Reset')
+for ii = 1:numPkts
+    b = randi([0 M-1],numDataSymbols,1);
+    dataSym = pskmod(b,M,pi/4);
+    packet = [trainingSymbols;dataSym];
+    channel = 1;
+    rx = awgn(packet*channel,SNR);
+    [~,err] = lineq(rx,trainingSymbols);
+    if (ii ==1 || ii == 2 ||ii == numPkts)
+        subplot(3,1,jj)
+        plot(abs(err))
+        title(['Packet # ',num2str(ii)])
+        xlabel('Symbols')
+        ylabel('Error Magnitude')
+        axis([0,length(packet),0,1])
+        grid on;
+        jj = jj+1;
+    end
+end
+
+
+%% New System to test
+M = 4;
+Rs = 1e6;
+fd = 20;
+spp = 200; % Symbols per packet
+b = randi([0 M-1],numDataSymbols,1);
+dataSym = pskmod(b,M,pi/4);
+packet = [trainingSymbols; dataSym];
+stream = repmat(packet,10,1);
+tx = (0:length(stream)-1)'/Rs;
+channel = exp(1i*2*pi*fd*tx);
+rx = awgn(stream.*channel,SNR);
+
+release(lineq)
+lineq.AdaptAfterTraining = false;
+
+[y,err] = lineq(rx,trainingSymbols);
+
+figure()
+subplot(2,1,1)
+plot(tx, unwrap(angle(channel)))
+xlabel('Time (sec)')
+ylabel('Channel Angle (rad)')
+title('Angular Error Over Time')
+subplot(2,1,2)
+plot(abs(err))
+xlabel('Symbols')
+ylabel('Error Magnitude')
+grid on
+title('Time-Varying Channel Without Retraining')
+
+scatterplot(y)
+
+
+release(lineq)
+lineq.TrainingFlagInputPort = true;
+symbolCnt = 0;
+numPackets = length(rx)/spp;
+trainFlag = true;
+trainingPeriod = 2000;
+eVec = zeros(size(rx));
+yVec = zeros(size(rx));
+for p=1:numPackets
+    [yVec((p-1)*spp+1:p*spp,1),eVec((p-1)*spp+1:p*spp,1)] = ...
+        lineq(rx((p-1)*spp+1:p*spp,1),trainingSymbols,trainFlag);
+    symbolCnt = symbolCnt + spp;
+    if symbolCnt >= trainingPeriod
+        trainFlag = true;
+        symbolCnt = 0;
+    else
+        trainFlag = false;
+    end
+end
+figure
+subplot(2,1,1)
+plot(tx, unwrap(angle(channel)))
+xlabel('t (sec)')
+ylabel('Channel Angle (rad)')
+title('Angular Error Over Time')
+subplot(2,1,2)
+plot(abs(eVec))
+xlabel('Symbols')
+ylabel('Error Magnitude')
+grid on
+title('Time-Varying Channel With Retraining')
+scatterplot(yVec)
