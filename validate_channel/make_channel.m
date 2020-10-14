13a10b22a64641abff915b5d7569b83397725ad3
Alex Salois <asalois.scholar@gmail.com>
2020-10-14 14:27:04 -0600

    example from Matlab Docs

--- validate_channel/make_channel.m
+++ validate_channel/make_channel.m
@@ -10,17 +10,26 @@ clear all;
 clc; 
 
 %% System Paramters
-Fs = 1000;
-t = linspace(0,1,4*Fs);
-x = cos(2*pi*10*t);
+Fs = 1;           % sampling frequency (notional)
+nBits = 2048;     % number of BPSK symbols per vector
+maxErrs = 200;    % target number of errors at each Eb/No
+maxBits = 1e6;    % maximum number of symbols at each Eb/No
 
 
-%% Channel Filter Paramters
-b = [1 -1];
-channel = filter(b,1,x);
+% Modulated signal parameters
+M = 2;                     % order of modulation
+Rs = Fs;                   % symbol rate
+nSamp = Fs/Rs;             % samples per symbol
+Rb = Rs*log2(M);           % bit rate
 
-figure()
-plot(t,x)
-figure()
-plot(t,channel)
+% Channel parameters
+chnl = [0.227 0.460 0.688 0.460 0.227]';  % channel impulse response
+chnlLen = length(chnl);                   % channel length, in samples
+EbNo = 0:14;                              % in dB
+BER = zeros(size(EbNo));                  % initialize values
 
+% Create BPSK modulator
+bpskMod = comm.BPSKModulator;
+
+% Specify a seed for the random number generators to ensure repeatability.
+rng(12345)
