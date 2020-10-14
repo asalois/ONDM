189b73f81d659d8d8914778397fee639ea2e2389
Alex Salois <asalois.scholar@gmail.com>
2020-10-13 18:28:13 -0600

    started to sim proakis channel

--- mmf_sim/adapt_eq.m
+++ mmf_sim/adapt_eq.m
@@ -9,109 +9,61 @@ clc;
 clear;
 close all;
 
-%% system setup
-M = 4;
+for i = 1:10
+% system setup
+M = 2; % mod depth
+Rs = 1e6; % symbol rate ?
+fd = 10;  % frequency shift delta
 numTrainSymbols = 200;
-numDataSymbols = 2000;
-SNR = 20;
+numDataSymbols = 1800;
+SNR = 20; % signal to Noise Ratio
 trainingSymbols = pskmod(randi([0 M-1],numTrainSymbols,1),M,pi/4);
-numPkts = 10;
+
+% eq setup
 lineq = comm.LinearEqualizer('Algorithm','LMS', ...
-    'NumTaps',10,'StepSize',0.01);
-
-%% system test with equalizer
-% train the equalizer at the beginning of each packet with reset
-jj = 1;
-figure()
-title('With Reset')
-for ii = 1:numPkts
-    b = randi([0 M-1],numDataSymbols,1);
-    dataSym = pskmod(b,M,pi/4);
-    packet = [trainingSymbols;dataSym];
-    rx = awgn(packet,SNR);
-    [~,err] = lineq(rx,trainingSymbols);
-    reset(lineq)
-    if (ii ==1 || ii == 2 ||ii == numPkts)
-        subplot(3,1,jj)
-        plot(abs(err))
-        title(['Packet # ',num2str(ii)])
-        xlabel('Symbols')
-        ylabel('Error Magnitude')
-        axis([0,length(packet),0,1])
-        grid on;
-        jj = jj+1;
-    end
-end
+    'NumTaps',8,'ReferenceTap',3,'StepSize',0.01);
 
 
-% Train the Equalizer at the Beginning of Each Packet Without Reset
-release(lineq)
-jj = 1;
-figure()
-title('Without Reset')
-for ii = 1:numPkts
-    b = randi([0 M-1],numDataSymbols,1);
-    dataSym = pskmod(b,M,pi/4);
-    packet = [trainingSymbols;dataSym];
-    channel = 1;
-    rx = awgn(packet*channel,SNR);
-    [~,err] = lineq(rx,trainingSymbols);
-    if (ii ==1 || ii == 2 ||ii == numPkts)
-        subplot(3,1,jj)
-        plot(abs(err))
-        title(['Packet # ',num2str(ii)])
-        xlabel('Symbols')
-        ylabel('Error Magnitude')
-        axis([0,length(packet),0,1])
-        grid on;
-        jj = jj+1;
-    end
-end
-
-
-%% add to system to test periodic changes
-Rs = 1e6;
-fd = 20;
-spp = 200; % Symbols per packet
-b = randi([0 M-1],numDataSymbols,1);
-dataSym = pskmod(b,M,pi/4);
-packet = [trainingSymbols; dataSym];
-stream = repmat(packet,10,1);
+b = randi([0 M-1],numDataSymbols,1); % random bit stream
+dataSym = pskmod(b,M,pi/4); % psk modulate
+packet = [trainingSymbols; dataSym]; % packet
+stream = repmat(packet,10,1); % bit stream of packets
 tx = (0:length(stream)-1)'/Rs;
-channel = exp(1i*2*pi*fd*tx);
-rx = awgn(stream.*channel,SNR);
+channel = exp(1i*2*pi*fd*tx); % create channel
+rx = awgn(stream.*channel,SNR); % make it niosy
 
 release(lineq)
 lineq.AdaptAfterTraining = false;
 
 [y,err] = lineq(rx,trainingSymbols);
 
-% plot results 
-figure()
-subplot(2,1,1)
-plot(tx, unwrap(angle(channel)))
-xlabel('Time (sec)')
-ylabel('Channel Angle (rad)')
-title('Angular Error Over Time')
-subplot(2,1,2)
-plot(abs(err))
-xlabel('Symbols')
-ylabel('Error Magnitude')
-grid on
-title('Time-Varying Channel Without Retraining')
-scatterplot(y)
+% % plot results 
+% figure()
+% subplot(2,1,1)
+% plot(tx, unwrap(angle(channel)))
+% xlabel('Time (sec)')
+% ylabel('Channel Angle (rad)')
+% title('Angular Error Over Time')
+% subplot(2,1,2)
+% plot(abs(err))
+% xlabel('Symbols')
+% ylabel('Error Magnitude')
+% grid on
+% title('Time-Varying Channel Without Retraining')
+% scatterplot(y)
 
 
-% perodic training
+%perodic training
+spp = 200; % Symbols per packet to proceess
 release(lineq)
 lineq.TrainingFlagInputPort = true;
 symbolCnt = 0;
-numPackets = length(rx)/spp;
+numProcess = length(rx)/spp;
 trainFlag = true;
-trainingPeriod = 2000;
+trainingPeriod = 2000; % this should be about the length of packet
 eVec = zeros(size(rx));
 yVec = zeros(size(rx));
-for p=1:numPackets
+for p=1:numProcess
     [yVec((p-1)*spp+1:p*spp,1),eVec((p-1)*spp+1:p*spp,1)] = ...
         lineq(rx((p-1)*spp+1:p*spp,1),trainingSymbols,trainFlag);
     symbolCnt = symbolCnt + spp;
@@ -124,16 +76,17 @@ for p=1:numPackets
 end
 
 % plot results
-figure
-subplot(2,1,1)
-plot(tx, unwrap(angle(channel)))
-xlabel('t (sec)')
-ylabel('Channel Angle (rad)')
-title('Angular Error Over Time')
-subplot(2,1,2)
-plot(abs(eVec))
-xlabel('Symbols')
-ylabel('Error Magnitude')
-grid on
-title('Time-Varying Channel With Retraining')
+% figure
+% subplot(2,1,1)
+% plot(tx, unwrap(angle(channel)))
+% xlabel('t (sec)')
+% ylabel('Channel Angle (rad)')
+% title('Angular Error Over Time')
+% subplot(2,1,2)
+% plot(abs(eVec))
+% xlabel('Symbols')
+% ylabel('Error Magnitude')
+% grid on
+% title('Time-Varying Channel With Retraining')
 scatterplot(yVec)
+end
---
+++ mmf_sim/validate.m
@@ -0,0 +1,63 @@
+% QAM-16 with polarization mode dispersion
+
+% Montana State University 
+% Electrical & Computer Engineering Department
+% Created by Alexander Salois
+% Modified by Ioannis Roudas (7/29/20)
+
+%% Preliminary commands 
+close all;
+clear all;
+clc; 
+
+%% Simulation parameters
+% Signal parameters
+M = 4; % Modulation order
+k = log2(M); % Bits/symbol
+n = 5000*k; % Transmitted bits
+nSamp = 4; % Samples per symbol
+SNR = 20; % Signal to noise ratio (dB)
+
+% Fiber parameters
+num_modes = 1; % number of modes that includes polarizations (smf w/ x y)
+num_fibers = 1; % number of fibers to simulate
+num_fiber_sections = 100; %number of fiber sections
+tau = 1/100000; % Differential group delay (DGD) between x,y SOPs per segment
+
+% Launch conditions
+phi = pi/4; % Phase difference between x,y SOPs 
+launch_jones_vector = [cos(phi); sin(phi)]; % Launch Jones vector
+
+
+%% Filter Paramters
+span = 50; % Filter span in symbols
+rolloff = 0.25; % Rolloff factor
+
+
+%% Create the raised cosine transmit and receive filters using the previously defined parameters.
+txfilter = comm.RaisedCosineTransmitFilter('RolloffFactor',rolloff, ...
+    'FilterSpanInSymbols',span,'OutputSamplesPerSymbol',nSamp);
+
+rxfilter = comm.RaisedCosineReceiveFilter('RolloffFactor',rolloff, ...
+    'FilterSpanInSymbols',span,'InputSamplesPerSymbol',nSamp, ...
+    'DecimationFactor',nSamp);
+
+
+%% Make Tx signal
+% Calculate the delay through the matched filters. 
+% The group delay is half of the filter span through one filter and is, 
+% therefore, equal to the filter span for both filters. 
+% Multiply by the number of bits per symbol to get the delay in bits.
+filtDelay = k*span; 
+errorRate = comm.ErrorRate('ReceiveDelay',filtDelay); % set error rate
+x = randi([0 1],n,1); % random bit stream 
+modSig = qammod(x, M,'InputType','bit'); % modulate
+txSig = txfilter(modSig); % filter modulted signal
+
+%% Tx spectrum
+nfft = 2^nextpow2(length(txSig));
+Y = fft(txSig,nfft); %  fft
+G = fftshift(Y); % zero-centered spectrum
+f = ((-nfft/2+1/2):(nfft/2-1/2))*nSamp; % zero-centered frequency range
+
+
