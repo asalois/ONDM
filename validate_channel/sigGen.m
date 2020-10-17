f646c1a1beb9815af697b6181f402d44dc51b196
Alex Salois <asalois.scholar@gmail.com>
2020-10-16 20:42:39 -0600

    simulating bpsk

--- validate_channel/sigGen.m
+++ validate_channel/sigGen.m
@@ -1,14 +1,22 @@
 %% Signal Generator
 
-function niosySig = sigGen(M, nBits, chnl, EbNo) 
+function [niosySig] = sigGen(M, nBits, chnl, EbNo, nsb, rb, fs) 
 
 % Generate a PSK signal
 msg = randi([0 M-1],nBits,1);
-txSig = pskmod(msg, M);
+symbols = pskmod(msg, M);
 
 % Pass the signal through the channel
-filtSig = filter(chnl,1,txSig);
+%filtSig = filter(chnl,1,symbols);
+
+% Make rectangular pulses
+%pulseSig = rectpulse(filtSig, nsb);
+pulseSig = rectpulse(symbols, nsb);
 
 % Add AWGN to the signal
-SNR = EbNo + 10*log10(Rb/Fs);
-noisySig = awgn(filtSig,SNR,'measured');
+%SNR = EbNo + 10*log10(rb/fs);
+%niosySig = awgn(pulseSig,SNR,'measured');
+niosySig = pulseSig;
+
+end
+
