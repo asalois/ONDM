8a19db6bd171f1530c9944f774187b66fdab85a6
Alexander Salois <asalois.scholar@gmail.com>
2020-10-19 14:32:03 -0600

    LMS added but does not perform well

--- validate_channel/lmsEq.m
+++ validate_channel/lmsEq.m
@@ -1,3 +1,17 @@
 %% Adaptive Linear Equilizer
 
+% Montana State University 
+% Electrical & Computer Engineering Department
+% Created by Alexander Salois based on https://www.mathworks.com/help/comm/ug/adaptive-equalizers.html#a1049736245
 
+% system setup
+numTrainSymbols = 200;
+trainingSymbols = filtSig(1:numTrainSymbols);
+
+% eq setup
+lineq = comm.LinearEqualizer('Algorithm','LMS', ...
+    'NumTaps',20,'ReferenceTap',10,'StepSize',0.01);
+
+[rxLMS,err] = lineq(filtSig,trainingSymbols);
+release(lineq)
+lineq.AdaptAfterTraining = false;
