6630d3c0d61aa16309add1dd448f74704f9c3432
Alexander Salois <asalois.scholar@gmail.com>
2020-10-19 15:28:26 -0600

    find the best number if taps

--- validate_channel/lmsEq.m
+++ validate_channel/lmsEq.m
@@ -3,14 +3,20 @@
 % Montana State University 
 % Electrical & Computer Engineering Department
 % Created by Alexander Salois based on https://www.mathworks.com/help/comm/ug/adaptive-equalizers.html#a1049736245
+function rxLMS = lmsEq(filtSig,i)
 
 % system setup
-numTrainSymbols = 200;
+numTrainSymbols = 2000;
 trainingSymbols = filtSig(1:numTrainSymbols);
 
 % eq setup
-lineq = comm.LinearEqualizer('Algorithm','LMS', ...
-    'NumTaps',20,'ReferenceTap',10,'StepSize',0.01);
+if i > 2
+	lineq = comm.LinearEqualizer('Algorithm','LMS', ...
+    	  'NumTaps',i,'ReferenceTap',floor(i/2),'StepSize',0.01);
+else
+	lineq = comm.LinearEqualizer('Algorithm','LMS', ...
+    	  'NumTaps',i,'ReferenceTap',1,'StepSize',0.01);
+end
 
 [rxLMS,err] = lineq(filtSig,trainingSymbols);
 release(lineq)
