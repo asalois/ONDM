21a464be6fba46fd3e0192d939d97896e80899e3
Alex Salois <asalois.scholar@gmail.com>
2020-10-20 15:46:23 -0600

    dfe implemeted in different function

--- validate_channel/lmsEq.m
+++ validate_channel/lmsEq.m
@@ -1,30 +1,22 @@
-%% Adaptive Linear Equilizer
-
+function rxLMS = lmsEq(filtSig,taps,numSymbols,PSKConstellation)
+% Adaptive Linear Equilizer using Least Mean Squares
 % Montana State University 
 % Electrical & Computer Engineering Department
 % Created by Alexander Salois
-
-function rxLMS = lmsEq(filtSig,i,numSymbols,PSKConstellation)
 
 % define number of symbols to train EQ
 numTrainSymbols = numSymbols;
 trainingSymbols = filtSig(1:numTrainSymbols);
 
 % eq setup
-if i > 1
-	%lineq = comm.LinearEqualizer('Algorithm','LMS', 'NumTaps',i,'ReferenceTap',1,'StepSize',0.03,'InputSamplesPerSymbol',1);
-    dfe = comm.DecisionFeedbackEqualizer('Algorithm','LMS','NumForwardTaps',i, ...
-        'NumFeedbackTaps', i-1,'StepSize',0.03,'ReferenceTap',1,'Constellation',PSKConstellation);
- 
+lineq = comm.LinearEqualizer('Algorithm','LMS', 'NumTaps',taps,'ReferenceTap',floor(taps/2),...
+    'StepSize',0.03,'InputSamplesPerSymbol',1, 'Constellation',PSKConstellation);
+
+% Use LMS Equalizer
+[rxLMS] = lineq(filtSig,trainingSymbols);
+
 end
 
-% Use LinearEq
-% [rxLMS,err] = lineq(filtSig,trainingSymbols);
-% release(lineq)
-% lineq.AdaptAfterTraining = false;
-
-% USE DFE 
-[rxLMS,err] =  dfe(filtSig,trainingSymbols);
 
 
 
--- validate_channel/main.m
+++ validate_channel/main.m
@@ -24,7 +24,7 @@ nsb=32; % Number of samples per bit
 fs=nsb*Rb; % Sampling frequency
 Ts=1/fs; % Sampling period
 Ttot=nb*Tb; % Total simulation time
-EbNo = 10; % Signal-to-noise energy ratio per bit Eb/N0 in linear units
+EbNo = 1000; % Signal-to-noise energy ratio per bit Eb/N0 in linear units
 SNR = 10*log10(EbNo); % + 10*log10(m) - 10*log10(ns); % Noise SNR per sample in (dB)
 
 % Modulated signal parameters
@@ -45,64 +45,55 @@ symbols = hPSKMod(msg);
 PSKConstellation = constellation(hPSKMod)'; % PSK constellation
 
 % Channel parameters
-chnl = [0.227 0.460 0.688 0.460 0.227];% channel impulse response
-%chnl = [0.25 0.5 0.25]; % another channel for testiing
+%chnl = [0.227 0.460 0.688 0.460 0.227];% channel impulse response
+chnl = [ 0.1 0.2 1 0.2 0.1]; % another channel for testiing
 chnlLen = length(chnl);                   % channel length, in samples
 
 % Pass the signal through the channel
 filtSig = filter(chnl,1,symbols);
 
 % Add AWGN to the signal
-% niosySig = awgn(filtSig,SNR,'measured');
+niosySig = awgn(filtSig,SNR,'measured');
 
-% variable used in loop below
-best = 1;
-
-for i =2:25
-
-% Use LMS     
-% rxLMS = lmsEq(niosySig,i,2000);
-rxLMS = lmsEq(filtSig,i,2000,PSKConstellation);
-rxSig = rxLMS;
+%% Use LMS     
+rxSig = lmsEq(niosySig,5,2000,PSKConstellation);
 bkEst = hPSKDemod(rxSig);
 
+% plot before and after
+scatterplot(niosySig(2001:end));
+scatterplot(rxSig(2001:end));
+
 % find BER
-[numErrors,ber] = biterr(msg(1:nb),bkEst(1:nb));
+[numErrors,berLMS] = biterr(msg(2002:nb-2),bkEst(2004:nb));
 
-% Print BER
-% fprintf('\n The number taps = %d', i)
-% 
-% fprintf('\n Bit error rate = %5.2e, based on %d errors\n', ...
-%     ber,numErrors)
+fprintf('\n LMS Bit error rate = %5.2e, based on %d errors\n', ...
+    berLMS,numErrors)
 
-    % check of the performance of equalizer improved
-    if ber < best
-        best = ber;
-        besti = i;
-        numErrs = numErrors;
-    end
+%% Use DFE   
+rxSig = dfEq(niosySig,5,2000,PSKConstellation);
+bkEst = hPSKDemod(rxSig);
 
-end
+% plot before and after
+scatterplot(niosySig(2001:end));
+scatterplot(rxSig(2001:end));
 
-% print the best result of the loop
-fprintf('\n The best number of taps = %d', besti)
+% find BER
+[numErrors,berDFE] = biterr(msg(2002:nb-2),bkEst(2004:nb));
 
-fprintf('\n Bit error rate = %5.2e, based on %d errors\n', ...
-    best,numErrs)
-
+fprintf('\n DFE Bit error rate = %5.2e, based on %d errors\n', ...
+    berDFE,numErrors)
 
 % Theoretical error probability
 peb = 0.5*erfc(sqrt(EbNo));
 
 % run with out LMS Equalizer
 rxSig = filtSig;
-bkEstNoLMS = pskdemod(rxSig,M);
+bkEstNoLMS =  hPSKDemod(rxSig);
 
 % BER with out LMS
-[numErrors,ber] = biterr(msg(1:nb),bkEstNoLMS(1:nb));
+[numErrors,ber] = biterr(msg(2002:nb-2),bkEstNoLMS(2004:nb));
 
 % print results
-fprintf('\n No LMS')
 
 fprintf('\n Bit error rate = %5.2e, based on %d errors', ...
     ber,numErrors)
