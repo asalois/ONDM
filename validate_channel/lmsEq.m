616558bf8b3c02cfb911f3a95ee7c61310618deb
Alex Salois <asalois.scholar@gmail.com>
2020-10-20 09:43:33 -0600

    added more comments

--- validate_channel/lmsEq.m
+++ validate_channel/lmsEq.m
@@ -2,27 +2,27 @@
 
 % Montana State University 
 % Electrical & Computer Engineering Department
-% Created by Alexander Salois based on https://www.mathworks.com/help/comm/ug/adaptive-equalizers.html#a1049736245
-function rxLMS = lmsEq(filtSig,i,M)
+% Created by Alexander Salois
 
-% system setup
-numTrainSymbols = 2000;
-% msg = randi([0 M-1],numTrainSymbols,1);
-% trainingSymbols = pskmod(msg,M);
+function rxLMS = lmsEq(filtSig,i,numSymbols)
+
+% define number of symbols to train EQ
+numTrainSymbols = numSymbols;
 trainingSymbols = filtSig(1:numTrainSymbols);
 
 % eq setup
 if i > 1
 	%lineq = comm.LinearEqualizer('Algorithm','LMS', 'NumTaps',i,'ReferenceTap',1,'StepSize',0.03,'InputSamplesPerSymbol',1);
     dfe = comm.DecisionFeedbackEqualizer('Algorithm','LMS','NumForwardTaps',i, 'NumFeedbackTaps', i-1,'StepSize',0.03,'ReferenceTap',1);
-else
-	%lineq = comm.LinearEqualizer('Algorithm','LMS', 'NumTaps',i,'ReferenceTap',1,'StepSize',0.01);
 end
 
+% Use LinearEq
 % [rxLMS,err] = lineq(filtSig,trainingSymbols);
 % release(lineq)
 % lineq.AdaptAfterTraining = false;
+
+% USE DFE 
 [rxLMS,err] =  dfe(filtSig,trainingSymbols);
-% release(dfe)
+
 
 
