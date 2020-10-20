139dedd0fb2b6932db5646ef5d6b4b3a332ea845
Alex Salois <asalois.scholar@gmail.com>
2020-10-20 16:02:03 -0600

    LMS and DFE work with niose

--- validate_channel/main.m
+++ validate_channel/main.m
@@ -15,7 +15,7 @@ tic
 
 % System simulation parameters
 Fs = 1; % sampling frequency (notional)
-nb = 2^19; % number of BPSK symbols per vector
+nb = 2^20; % number of BPSK symbols per vector
 Tb=1; % Bit period
 Rb=1/Tb; % Bit rate
 fc=2; % Carrier frequency
@@ -24,7 +24,7 @@ nsb=32; % Number of samples per bit
 fs=nsb*Rb; % Sampling frequency
 Ts=1/fs; % Sampling period
 Ttot=nb*Tb; % Total simulation time
-EbNo = 1000; % Signal-to-noise energy ratio per bit Eb/N0 in linear units
+EbNo = 10; % Signal-to-noise energy ratio per bit Eb/N0 in linear units
 SNR = 10*log10(EbNo); % + 10*log10(m) - 10*log10(ns); % Noise SNR per sample in (dB)
 
 % Modulated signal parameters
@@ -41,63 +41,58 @@ hPSKMod   = comm.PSKModulator(2, ...
 hPSKDemod = comm.PSKDemodulator(2, ...
     'PhaseOffset',0, ...
     'SymbolMapping','Binary');
-symbols = hPSKMod(msg);
+symbols = hPSKMod(msg); % symbols created
 PSKConstellation = constellation(hPSKMod)'; % PSK constellation
 
 % Channel parameters
 %chnl = [0.227 0.460 0.688 0.460 0.227];% channel impulse response
 chnl = [ 0.1 0.2 1 0.2 0.1]; % another channel for testiing
-chnlLen = length(chnl);                   % channel length, in samples
+chnlLen = length(chnl); % channel length, in samples
 
 % Pass the signal through the channel
 filtSig = filter(chnl,1,symbols);
 
 % Add AWGN to the signal
 niosySig = awgn(filtSig,SNR,'measured');
+inputSig = niosySig;
 
 %% Use LMS     
-rxSig = lmsEq(niosySig,5,2000,PSKConstellation);
+rxSig = lmsEq(inputSig,5,2000,PSKConstellation);
 bkEst = hPSKDemod(rxSig);
 
-% plot before and after
-scatterplot(niosySig(2001:end));
-scatterplot(rxSig(2001:end));
-
 % find BER
-[numErrors,berLMS] = biterr(msg(2002:nb-2),bkEst(2004:nb));
+delay = 3;
+[numErrors,berLMS] = biterr(msg(2000:nb-delay),bkEst(2000+delay:nb));
 
-fprintf('\n LMS Bit error rate = %5.2e, based on %d errors\n', ...
+fprintf('LMS Bit error rate = %5.2e, based on %d errors\n', ...
     berLMS,numErrors)
 
 %% Use DFE   
-rxSig = dfEq(niosySig,5,2000,PSKConstellation);
+rxSig = dfEq(inputSig,5,2000,PSKConstellation);
 bkEst = hPSKDemod(rxSig);
 
-% plot before and after
-scatterplot(niosySig(2001:end));
-scatterplot(rxSig(2001:end));
-
 % find BER
-[numErrors,berDFE] = biterr(msg(2002:nb-2),bkEst(2004:nb));
+[numErrors,berDFE] = biterr(msg(2000:nb-delay),bkEst(2000+delay:nb));
 
-fprintf('\n DFE Bit error rate = %5.2e, based on %d errors\n', ...
+fprintf('DFE Bit error rate = %5.2e, based on %d errors\n', ...
     berDFE,numErrors)
 
 % Theoretical error probability
 peb = 0.5*erfc(sqrt(EbNo));
 
 % run with out LMS Equalizer
-rxSig = filtSig;
+rxSig = inputSig;
 bkEstNoLMS =  hPSKDemod(rxSig);
 
 % BER with out LMS
-[numErrors,ber] = biterr(msg(2002:nb-2),bkEstNoLMS(2004:nb));
+delay = delay -1;
+[numErrors,ber] = biterr(msg(2000:nb-delay),bkEstNoLMS(2000+delay:nb));
 
 % print results
 
-fprintf('\n Bit error rate = %5.2e, based on %d errors', ...
+fprintf('No EQ Bit error rate = %5.2e, based on %d errors\n', ...
     ber,numErrors)
 
-fprintf('\n Theoretical error probability Peb = %5.2e\n', ...
+fprintf('Theoretical error probability Peb = %5.2e\n\n', ...
     peb)
 toc
