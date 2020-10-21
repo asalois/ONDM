71abfd158b548b3a5feb0efa9de9c6bd9916c439
Alex Salois <asalois.scholar@gmail.com>
2020-10-21 12:45:19 -0600

    generate BER plots

--- validate_channel/main.m
+++ validate_channel/main.m
@@ -12,10 +12,9 @@ close all;
 % time simulation
 tic
 %% Signal and Channel Parameters
-
 % System simulation parameters
 Fs = 1; % sampling frequency (notional)
-nb = 2^20; % number of BPSK symbols per vector
+nb = 2^22; % number of BPSK symbols per vector
 Tb=1; % Bit period
 Rb=1/Tb; % Bit rate
 fc=2; % Carrier frequency
@@ -24,8 +23,7 @@ nsb=32; % Number of samples per bit
 fs=nsb*Rb; % Sampling frequency
 Ts=1/fs; % Sampling period
 Ttot=nb*Tb; % Total simulation time
-EbNo = 10; % Signal-to-noise energy ratio per bit Eb/N0 in linear units
-SNR = 10*log10(EbNo); % + 10*log10(m) - 10*log10(ns); % Noise SNR per sample in (dB)
+EbNo = 1; % Signal-to-noise energy ratio per bit Eb/N0 in linear units
 
 % Modulated signal parameters
 M = 2; % order of modulation
@@ -52,47 +50,65 @@ chnlLen = length(chnl); % channel length, in samples
 % Pass the signal through the channel
 filtSig = filter(chnl,1,symbols);
 
+% Loop Set up
+runTo = 25;
+step = 0.01;
+runs = runTo/step;
+berR = zeros(4,runs);
+snrPlot =  1*step:step:runTo;
+
+for i = 1:runs
+SNR = i*step; % Noise SNR per sample in (dB)
+
 % Add AWGN to the signal
 niosySig = awgn(filtSig,SNR,'measured');
 inputSig = niosySig;
 
-%% Use LMS     
-rxSig = lmsEq(inputSig,5,2000,PSKConstellation);
+%% Use LMS
+trainNum = 2000;
+taps = 5;
+rxSig = lmsEq(inputSig,taps,trainNum,PSKConstellation);
 bkEst = hPSKDemod(rxSig);
 
 % find BER
 delay = 3;
-[numErrors,berLMS] = biterr(msg(2000:nb-delay),bkEst(2000+delay:nb));
+[numErrors,berLMS] = biterr(msg(trainNum:nb-delay),bkEst(trainNum+delay:nb));
 
-fprintf('LMS EQ Bit error rate = %5.2e, based on %d errors\n', ...
-    berLMS,numErrors)
 
 %% Use DFE   
-rxSig = dfEq(inputSig,5,2000,PSKConstellation);
+rxSig = dfEq(inputSig,taps,trainNum,PSKConstellation);
 bkEst = hPSKDemod(rxSig);
 
 % find BER
-[numErrors,berDFE] = biterr(msg(2000:nb-delay),bkEst(2000+delay:nb));
-
-fprintf('DF EQ  Bit error rate = %5.2e, based on %d errors\n', ...
-    berDFE,numErrors)
+[numErrors,berDFE] = biterr(msg(trainNum:nb-delay),bkEst(trainNum+delay:nb));
 
 % Theoretical error probability
-peb = 0.5*erfc(sqrt(EbNo));
+peb = 0.5*erfc(sqrt(10^SNR/10));
 
-% run with out LMS Equalizer
+%% run with out LMS Equalizer
 rxSig = inputSig;
 bkEstNoLMS =  hPSKDemod(rxSig);
 
 % BER with out LMS
 delay = delay -1;
-[numErrors,ber] = biterr(msg(2000:nb-delay),bkEstNoLMS(2000+delay:nb));
+[numErrors,ber] = biterr(msg(trainNum:nb-delay),bkEstNoLMS(trainNum+delay:nb));
 
-% print results
-
-fprintf('No EQ  Bit error rate = %5.2e, based on %d errors\n', ...
-    ber,numErrors)
-
-fprintf('Theoretical error probability Peb = %5.2e\n\n', ...
-    peb)
+% hold values in berR
+berR(:,i) = [ber,berDFE,berLMS,peb];
+end
 toc
+
+%%
+figure()
+plot(snrPlot,berR(1,:),snrPlot,berR(3,:),snrPlot,berR(2,:),snrPlot,berR(4,:))
+legend('No EQ','LMS EQ','DFE EQ','Theoretical');
+xlabel('SNR (dB)');
+ylabel('BER');
+saveas(gcf,'BER.png');
+
+figure()
+semilogy(snrPlot,berR(1,:),snrPlot,berR(3,:),snrPlot,berR(2,:));
+legend('No EQ','LMS EQ','DFE EQ');
+xlabel('SNR (dB)');
+ylabel('BER');
+saveas(gcf,'BERlogy.png');
