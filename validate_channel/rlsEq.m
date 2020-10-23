42c8ef3e515cc4fc7bc6951415bdc1ed1c4dddb4
Alexander Salois <asalois.scholar@gmail.com>
2020-10-22 20:02:02 -0600

    added rls

---
+++ validate_channel/rlsEq.m
@@ -0,0 +1,19 @@
+function rxRLS = rlsEq(filtSig,taps,numSymbols)
+% Adaptive Linear Equilizer using Least Mean Squares
+% Montana State University 
+% Electrical & Computer Engineering Department
+% Created by Alexander Salois
+
+% define number of symbols to train EQ
+numTrainSymbols = numSymbols;
+trainingSymbols = filtSig(1:numTrainSymbols);
+
+% eq setup
+lineq = comm.LinearEqualizer('Algorithm','LMS', 'NumTaps',taps,'ReferenceTap',floor(taps/2),...
+    'InputSamplesPerSymbol',1,'Constellation',pskmod(0:1,2));
+
+% Use LMS Equalizer
+[rxRLS] = lineq(filtSig,trainingSymbols);
+
+end
+
