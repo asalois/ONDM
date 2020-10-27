ecb8280e3d13c7287d5f9c045c196fcc81158baf
Alex Salois <asalois.scholar@gmail.com>
2020-10-27 11:47:54 -0600

    make bigger inputs

---
+++ validate_channel/makeInputMat.m
@@ -0,0 +1,11 @@
+function mat = makeInputMat(input,numSamples,numTrain);
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+l = 2*numSamples + 1;
+mat = zeros(l,numTrain);
+for i = 1:l
+    mat(i,:) = input(i:numTrain + (i-1)); 
+end
+mat = [real(mat); imag(mat)];
+end
+
