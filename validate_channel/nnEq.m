1cbaad62699cfd4ae6e63954120b1e186ecdc670
Alexander Salois <asalois.scholar@gmail.com>
2020-11-03 17:32:21 -0700

    tinkering with NN

--- validate_channel/nnEq.m
+++ validate_channel/nnEq.m
@@ -1,19 +1,19 @@
 %% Neural Network for channel equaliztion
 
-% Montana State University 
+% Montana State University
 % Electrical & Computer Engineering Department
 % Created by Alexander Salois
 function output = nnEq(input,target,numTrain)
-size = 14;
+size = 25;
 hLayers = round(size*4*1.25);
 shift = size+1;
 trainingSymbols = makeInputMat(input,size,numTrain);
 target = [real(target(shift:numTrain+shift-1)), imag(target(shift:numTrain+shift-1))];
 testData = makeInputMat(input,size,(length(input) - numTrain));
 
-Eqnet = patternnet(hLayers,'traingd');
-Eqnet.layers{2}.transferFcn = 'purelin';
-% Eqnet = feedforwardnet(hLayers,'traingd');
+% Eqnet = patternnet(hLayers,'traingd');
+% Eqnet.layers{2}.transferFcn = 'purelin';
+Eqnet = feedforwardnet(hLayers,'traingd');
 % Eqnet = patternnet(hLayers);
 % Train the Network
 [Eqnet,TT] = train(Eqnet,trainingSymbols,target','useGPU', 'yes');
