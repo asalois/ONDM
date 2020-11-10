18c72885f3d7cc58401f23dab4e884b0fc4bfb70
Alexander Salois <asalois.scholar@gmail.com>
2020-11-10 12:51:15 -0700

    runs faster on cpu

--- validate_channel/shiftCheck.m
+++ validate_channel/shiftCheck.m
@@ -1,10 +1,18 @@
-function shift = shiftCheck(in1,in2)
+function shift = shiftCheck(in1,in2,runTo)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-gpu1 = gpuArray(in1(1:length(in2)));
-gpu2 = gpuArray(in2);
-x = gpuArray(zeros(length(in1),1));
-parfor i = 1:round(length(in1)/2)
+
+% to use gpu
+% gpu1 = gpuArray(in1(1:length(in2)));
+% gpu2 = gpuArray(in2);
+% x = gpuArray(zeros(length(in1),1));
+
+% to not use gpu
+gpu1 = in1(1:length(in2));
+gpu2 = in2;
+x = zeros(length(in1),1);
+
+parfor i = 1:runTo
     test = circshift(gpu1,i);
     n = dot(test,gpu2);
     x(i) = n;
