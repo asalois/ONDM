252539758ca479bc0c7c31b1c58172762ad0789f
Alexander Salois <asalois.scholar@gmail.com>
2020-11-11 09:50:53 -0700

    save the nn at each run then retrain

--- validate_channel/nnEq.m
+++ validate_channel/nnEq.m
@@ -11,11 +11,11 @@ trainingSymbols = makeInputMat(input,size,numTrain);
 test = input(numTrain+1:end);
 target = [real(target(shift:numTrain+shift-1)), imag(target(shift:numTrain+shift-1))];
 testData = makeInputMat(test,size,length(test)-(2*size+1));
-
-Eqnet = feedforwardnet(hLayers,'traingd');
+load Eqnet
+% Eqnet = feedforwardnet(hLayers,'traingd');
 % Eqnet = patternnet(hLayers,'traingd');
 % Eqnet.layers{2}.transferFcn = 'purelin';
-Eqnet.layers{1}.transferFcn = 'purelin';
+% Eqnet.layers{1}.transferFcn = 'purelin';
 % Eqnet = patternnet(hLayers);
 % Train the Network
 [Eqnet,TT] = train(Eqnet,trainingSymbols,target','useGPU', 'yes');
@@ -24,4 +24,5 @@ Eqnet.layers{1}.transferFcn = 'purelin';
 % Test the Network
 output = Eqnet(testData);
 output = [output(1,:) + output(2,:)*1i]';
+save Eqnet
 end
