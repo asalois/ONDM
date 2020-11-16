f7579eccea4f13dc35d1e7cd328aa709ef0c3315
Alexander Salois <asalois.scholar@gmail.com>
2020-11-16 14:46:37 -0700

    try pytorch

---
+++ implemnet_python/try_pytorch.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import torch
+x = torch.empty(5, 3)
+print(x)
+x = torch.rand(5, 3)
+print(x)
+x = torch.zeros(5, 3, dtype=torch.long)
+print(x)
+x = torch.tensor([5.5, 3])
+print(x)
