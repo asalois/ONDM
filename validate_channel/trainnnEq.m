5fe8ad31d87d29710fbb3135d666a0d54edfe9d7
Alexander Salois <asalois.scholar@gmail.com>
2020-11-19 16:23:38 -0700

    gave intial shift

--- validate_channel/trainnnEq.m
+++ validate_channel/trainnnEq.m
@@ -14,7 +14,7 @@ tic
 %% Signal and Channel Parameters
 % System simulation parameters
 Fs = 1; % sampling frequency (notional)
-nb = 2^23; % number of BPSK symbols per vector
+nb = 2^10; % number of BPSK symbols per vector
 
 % Modulated signal parameters
 M = 4; % order of modulation
@@ -26,7 +26,7 @@ berNN = 1;
 while berNN > 0.26
     % Generate a PSK signal
     msg = randi([0 M-1],nb,1);
-    symbols = pskmod(msg,M);
+    symbols = pskmod(msg,M,pi/8);
     
     % Channel parameters
     %chnl = [0.227 0.460 0.688 0.460 0.227];% channel impulse response
@@ -47,11 +47,11 @@ while berNN > 0.26
     trainNN = length(symbols)/4;
     perCent = trainNN/length(symbols);
     rx5Sig = nnEq(filtSig,symbols,trainNN);
-    bkEst = pskdemod(rx5Sig,M);
-    shift = shiftCheck(msg(trainNN:end-1),bkEst, 100)
+    bkEst = pskdemod(rx5Sig,M,pi/8);
+%     shift = shiftCheck(msg(trainNN:end-1),bkEst, 100)
     shift = 0;
-    x = msg(trainNN+29:end-1);
-    y = circshift(bkEst,-shift);
+    x = msg(trainNN:end-1);
+    y = circshift(bkEst,shift);
     diff = length(x) - length(y);
     diff
     % find BER
