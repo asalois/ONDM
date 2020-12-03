c2525ab8c7135c802b9fc53c3228325e671a91eb
Alex Salois <asalois.scholar@gmail.com>
2020-12-03 15:39:00 -0700

    lets train this another way

--- validate_channel/trainnnEq.m
+++ validate_channel/trainnnEq.m
@@ -11,45 +11,41 @@ close all;
 
 % time simulation
 tic
-%% Signal and Channel Parameters
-% System simulation parameters
-Fs = 1; % sampling frequency (notional)
-nb = 2^15; % number of symbols per vector
-
-% Modulated signal parameters
-M = 4; % order of modulation
 
 % Specify a seed for the random number generators to ensure repeatability.
 rng(12345)
 berNN = 1;
 
+data = csvread('data.csv');
+target = csvread('target.csv');
+
+%%
+% Define network
+hLayers = 70; % hidden layer size
+Eqnet = fitnet(hLayers,'traingd'); % make a fitnet
+Eqnet.layers{1}.transferFcn = 'purelin'; % have the actuvation be linear
+
+% Train the Network
+% [Eqnet,TT] = train(Eqnet,trainingSymbols,target','useGPU', 'yes'); % use when gpu
+[Eqnet,TT] = train(Eqnet,data,target); % use when no gpu and small data
+% [Eqnet,TT] = train(Eqnet,data,target,'useParallel','yes'); % use when no gpu and large data
+
+% Test the Network
+output = Eqnet(testData);
+output = [output(1,:) + output(2,:)*1i]';
+save Eqnet
+
 while berNN > 0.49
-    % Generate a signal
-    msg = randi([0 M-1],nb,1);
-    symbols = qammod(msg,M);
-    
-    % Channel parameters
-    %chnl = [0.227 0.460 0.688 0.460 0.227];% channel impulse response
-    %chnl = [0.04 -0.05 0.07 -0.21 -0.5 0.72 0.36 0.21 0.03 0.07]; % another channel for testing
-    chnl = [0 1 0]; % another channel for testing
-    %chnl = [0.407 0.815 0.407];
-%     chnlLen = length(chnl); % channel length, in samples
-    
-    % Pass the signal through the channel
-    filtSig = filter(chnl,1,symbols);
-    
-    % Add AWGN to the signal
-    SNR = 200;
-    niosySig = awgn(filtSig,SNR,'measured');
-    inputSig = niosySig;
-    
+
     % Use NN
     trainNN = length(symbols)/2;
     perCent = trainNN/length(symbols);
-%     symbols = circshift(symbols,-2);
+    symbols = circshift(symbols,-1);
     rx5Sig = nnEq(filtSig,symbols,trainNN);
+    sr = shiftCheck(rx5Sig,symbols(trainNN+1:end), length(rx5Sig))
+    sl = length(rx5Sig) - sr
     bkEst = qamdemod(rx5Sig,M);
-%     shift = shiftCheck(msg(trainNN:end-1),bkEst, 2^8)
+%     shift = shiftCheck(msg(trainNN+1:end),bkEst, 2^8)   
     shift = 0;
     x = msg(trainNN:end-1);
     y = circshift(bkEst,shift);
