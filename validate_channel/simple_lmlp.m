2177ad4876282965bc8d1dd63a2d0d953436d047
Alexander Salois <asalois.scholar@gmail.com>
2020-12-08 16:10:22 -0700

    use a test data set

--- validate_channel/simple_lmlp.m
+++ validate_channel/simple_lmlp.m
@@ -14,9 +14,9 @@ tic
 
 % Specify a seed for the random number generators to ensure repeatability.
 rng(12345)
-d = load('data.mat');
+d = load('data_test.mat');
 d.data = d.data(1:29,:);
-t = load('target.mat');
+t = load('target_test.mat');
 test = 2^15;
 % diff  = length(d.data) - length(t.target)
 size(d.data)
@@ -25,21 +25,21 @@ size(t.target)
 %%
 % Define network
 hLayers = 70/2; % hidden layer size
-Eqnet = fitnet(hLayers,'traingd'); % make a fitnet
-Eqnet.layers{1}.transferFcn = 'purelin'; % have the actuvation be linear
+Eq_simple_net = fitnet(hLayers,'traingd'); % make a fitnet
+Eq_simple_net.layers{1}.transferFcn = 'purelin'; % have the actuvation be linear
 
 % Train the Network
-[Eqnet,TT] = train(Eqnet,d.data(:,1:end-test),t.target(1:end-test),'useGPU', 'yes'); % use when gpu
+[Eq_simple_net,TT] = train(Eq_simple_net,d.data(:,1:end-test),t.target(1:end-test),'useGPU', 'yes'); % use when gpu
 % [Eqnet,TT] = train(Eqnet,d.data,t.target); % use when no gpu and small data
 % [Eqnet,TT] = train(Eqnet,d.data,t.target,'useParallel','yes'); % use when no gpu and large data
 
 %% Test the Network
-output = Eqnet(d.data(:,end-test:end));
+output = Eq_simple_net(d.data(:,end-test:end));
 err = output == t.target(end-test:end);
 sum(err)
 perCentErr = sum(err)/test
 % output = [output(1,:) + output(2,:)*1i]';
-save Eqnet
+save Eq_simple_net
 
 %%
 
