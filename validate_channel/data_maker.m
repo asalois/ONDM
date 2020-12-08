08ece125fedc8323fd0cab6bed225384b67fc049
Alex Salois <asalois.scholar@gmail.com>
2020-12-08 13:36:38 -0700

    lets use this to make data and to align the data with the target

--- validate_channel/data_maker.m
+++ validate_channel/data_maker.m
@@ -11,7 +11,7 @@ close all;
 
 tic
 
-nb = 2^20;
+nb = 2^22;
 
 % Modulated signal parameters
 M = 4; % order of modulation
@@ -22,24 +22,27 @@ rng(12345)
 % Generate a PSK signal
 msg = randi([0 M-1],nb,1);
 %symbols = pskmod(msg,M,pi/4);
-symbols = qammod(msg,M);
+symbols = msg;
 
 % Channel parameters
 %chnl = [0.227 0.460 0.688 0.460 0.227];% channel impulse response
 %chnl = [0.04 -0.05 0.07 -0.21 -0.5 0.72 0.36 0.21 0.03 0.07]; % another channel for testing
-%chnl = [ 0.1 0.2 1 0.2 0.1]; % another channel for testing
+% chnl = [ 0.1 0.2 1 0.2 0.1]; % another channel for testing
 chnl = [0.407 0.815 0.407];
 % chnl = [0 1 0]; % another channel for testing
 
 % Pass the signal through the channel
 filtSig = filter(chnl,1,symbols);
+shift = 1;
+filtSig = filtSig(1+shift:end);
 
 numSamples = 14;
 
-data = makeInputMat(filtSig,numSamples,nb);
+data = makeInputMat(filtSig,numSamples,nb-shift);
 data = data(:,1:end-numSamples);
 symbols = circshift(symbols, 0);
-target = round([real(symbols(numSamples+1:end)) imag(symbols(numSamples+1:end))],3)';
+% target = round([real(symbols(numSamples+1:end)) imag(symbols(numSamples+1:end))],3)';
+target = round(real(symbols(numSamples+1:end-shift)),3)';
 diff  = length(data) - length(target)
 
 save('data','data');
